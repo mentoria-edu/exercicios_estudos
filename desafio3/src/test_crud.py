b886ce9e98ee0d24667bb771ea65817c35bff202
Leo-Adelmo <leonardo-adelmo@hotmail.com>
2025-06-03 20:21:52 -0300

    Adicionando tramaento de erros e DocStings

--- desafio3/src/test_crud.py
+++ desafio3/src/test_crud.py
@@ -1,5 +1,5 @@
 from src.menu.menu import run_menu
-from src.model.models import init_db, insert_data, read_data, update_data, delete_data
+from src.model.models import init_db, insert_data, read_all_data, update_data, delete_data
 from faker import Faker
 
 fake = Faker('pt_br')
@@ -8,14 +8,7 @@ data = (fake.name(), fake.phone_number(), fake.email())
 
 init_db()
 insert_data(data)
-insert_data(data)
-print("Antes das mudanças \n")
-for row in read_data():
-    print(row)
-
-print("\nDepois das mudanças \n")
-update_data(id=1, field='name', new_value="Novo Nome")
-delete_data(id=2)
-
-for row in read_data():
+update_data(id=9, field='name', new_value="Leonardo7")
+delete_data(id=10)
+for row in read_all_data():
     print(row)
