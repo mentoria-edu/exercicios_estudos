11afa72913421372e19f9d494dd4b631ed5b3208
danilo <lisboadx@gmail.com>
2025-06-03 20:13:29 +0000

    inserindo arquivos para revisar

---
+++ desafio3/src/button/__init__.py
@@ -0,0 +1,15 @@
+"""buttons package initializer."""
+
+from adding_contact.insert_name import insert_name
+from adding_contact.insert_phone import insert_phone
+from adding_contact.insert_email import insert_email
+from adding_contact.create_contact import create_contact
+
+
+contact_informations = list()
+contact_informations.append(insert_name())
+contact_informations.append(insert_phone())
+contact_informations.append(insert_email())
+
+
+print(tuple(contact_informations))
