6fd16b05f6d90963276d9b95722261e3797bce91
danilo <lisboadx@gmail.com>
2025-06-03 20:16:44 +0000

    feat/iserindo pasta search_contact

---
+++ desafio3/src/button/search_contact/search_contact.py
@@ -0,0 +1,7 @@
+
+def search_for_a_contact():
+    search = input("Buscar: ")
+
+    for contact in contact_list:
+        if search in contact["nome"]:
+            print(f"{contact['nome']} - {contact['telefone']}")
