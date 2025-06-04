eb7592a2d1ea3dc84c0c09023a15eb928c7a9931
danilo <lisboadx@gmail.com>
2025-06-04 02:11:03 +0000

    fix/retirando conversao para strings no retorno da funcao

--- desafio3/src/button/adding_contact/insert_phone.py
+++ desafio3/src/button/adding_contact/insert_phone.py
@@ -1,5 +1,3 @@
-
-
 def insert_phone() -> str:
     area_code = input("Código de área (DDD): ").strip()
     if not area_code.isdigit() or len(area_code) != 2:
@@ -9,4 +7,4 @@ def insert_phone() -> str:
     if not phone.isdigit() or len(phone) != 8:
         raise ValueError("O telefone precisa deve ter oito dígitos.")
 
-    return str(area_code) + str(phone)
+    return area_code + phone
