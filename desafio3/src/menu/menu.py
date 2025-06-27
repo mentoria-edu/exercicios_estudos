4a73e47b7a51ea849e35268a775fd71390f76412
luiz-vaz <luiz.henrique.vaz@hotmail.com>
2025-06-26 21:23:34 -0300

    ajustando os arquivos do PR

--- desafio3/src/menu/menu.py
+++ desafio3/src/menu/menu.py
@@ -1,33 +1,109 @@
-def display_menu():
-    print("=== Menu Inicial ===")
-    print("1. Opção 1 - Descrição da opção 1")
-    print("2. Opção 2 - Descrição da opção 2")
-    print("3. Opção 3 - Descrição da opção 3")
-    print("4. Sair")
-    print("====================")
+from utils.logger import logger
+from functools import wraps
 
 
-def menu_options():
+def menu_loop(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        while True:
+            result = func(*args, **kwargs)
+
+            if result == '9':
+                break
+
+        return result
+      
+    return wrapper
+
+
+def show_display_main_menu():
+    logger.info("=== Menu ===")
+    logger.info("1. Add Contact")
+    logger.info("2. View All Contacts")
+    logger.info("3. Select Contact") 
+    logger.info("9. Exit")
+    logger.info("====================")
+
+
+def show_display_contact_submenu():
+    logger.info("=== Contact Menu ===")
+    logger.info("1. Update Contact")
+    logger.info("2. Delete Contact")
+    logger.info("9. Return")
+    logger.info("====================")
+
+
+def show_display_get_serch():
+    logger.info("Select the field to search by:")
+    logger.info("1. Name")
+    logger.info("2. Phone")
+    logger.info("3. E-mail")
+
+
+@menu_loop
+def execute_main_menu() -> str:
+    show_display_main_menu()
+
+    choice = input("Enter your choice: ")
+
+    match choice:
+        case '1':            
+            logger.info("Adding Contact...")                 
+        case '2':
+            logger.info("Viewing Contact...")                    
+        case '3':
+            logger.info("Selecting Contact...")
+            get_search_contact()           
+            execute_contact_submenu()       
+        case '9':
+            logger.info("Exiting...")
+            return '9'
+        case _:
+            logger.warning("Invalid option, check the options in the menu below")
+                        
+
+@menu_loop
+def execute_contact_submenu() -> str:
+    show_display_contact_submenu()
+
+    choice = input("Enter your choice: ")
+
+    match choice:
+        case '1':
+            logger.info("1. Update Contact")                  
+        case '2':
+            logger.info("2. Delete Contact")   
+        case '9':
+            logger.info("Returning...")
+            return '9'
+        case _:
+            logger.warning("Invalid option, check the options in the menu below")
+
+
+def get_field_search() -> str:
     while True:
-        choice = input("Enter your choice: ")
-        
-        if choice == '1':
-            print("Adding a contact...")
-            # Add contact logic here
-        elif choice == '2':
-            print("Viewing contacts...")
-            # View contacts logic here
-        elif choice == '3':
-            print("Searching for a contact...")
-            # Search contact logic here
-        elif choice == '4':
-            print("Deleting a contact...")
-            # Delete contact logic here
-        elif choice == '5':
-            print("Exiting the program.")
-            break
+        show_display_get_serch()
+
+        choice = input("Choice:")
+
+        match choice:            
+            case '1':
+                return 'name'
+            case '2':
+                return 'phone'             
+            case '3':
+                return 'email'      
+            case _:
+                logger.warning("Invalid option, check the options in the menu below")
+
+
+def get_search_contact() -> tuple[str, str]:
+    field = get_field_search()
+
+    search_value = input("Search:").strip()
+
+    return field, search_value
 
 
 def run_menu():
-    display_menu()
-    menu_options()
+    execute_main_menu()
